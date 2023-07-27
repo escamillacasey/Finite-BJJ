3aefd84aec763388f61bb4306a491041aa3790d9
Casey Escamilla <escamillacasey@gmail.com>
2023-07-26 20:58:02 -0400

    Initial Commit

---
+++ bjjAutomata.py
@@ -0,0 +1,22 @@
+'''
+BJJ Flow Chart Generator
+@author Casey Escamilla
+@date 20230726
+Purpose: Display in an easily understandable visual format a gameplan which identifies 
+holes and strong points as well as areas to be focused on in order to create a more sound game.
+'''
+
+# read xml
+## is xml the best format?
+## json?
+## sql?
+### database fields
+### position start
+### position end
+### move name
+#### position = (top/bottom/neutral) (guard/mount/submit) (defending/attacking/neutral) 
+
+# visualize
+# pyplot?
+## options
+## transitions from positions
